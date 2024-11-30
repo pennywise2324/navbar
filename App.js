19418d0de03625032e06f1c7cffb2b9057a3d422
maufal ardian <naufal.ard.2@gmail.com>
2024-11-30 18:54:14 +0700

    navbar

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('./assets/images/home.png')}
+          style={styles.image}
+        />
+        
+      </View>
+      <View style={styles.navbar}>
+        <Text style={styles.navText}>Beranda</Text>
+        <Text style={styles.navText}>Peringkat</Text>
+        <Text style={styles.navText}>Geotag</Text>
+        <Text style={styles.navText}>Kebugaran</Text>
+        <Text style={styles.navText}>Permainan</Text>
+        <Text style={styles.navText}>Profil</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +28,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end', 
   },
+  navbar: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly', 
+    alignItems: 'center',
+    paddingVertical: 10, 
+    
+  },
+  navText: {
+    fontSize: 10, 
+    fontWeight: '500', 
+    color: '#333', 
+  },
+  imageContainer:{
+    flexDirection:'row',
+    justifyContent: 'space-evenly',
+  },
+  image : {
+    width: 18,
+    height: 18,
+    resizeMode:'contain'
+  }
 });
